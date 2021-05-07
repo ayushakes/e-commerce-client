d6164361d80d40c2648e32217c5bbd1642d898d4
ayushakes <ayu.ayu.sharma@gmail.com>
2021-05-07 19:26:05 +0530

    firebase auth added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 
+import 'react-toastify/dist/ReactToastify.css';
+
 import Home from "./pages/home";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/nav/header";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (<>
   <Header />
+  <ToastContainer />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,9 +1,27 @@
 import React from "react";
-import {auth} from '../../firebase';
+import { auth } from "../../firebase";
+import { toast } from "react-toastify";
 
 export default function Register() {
   const [email, setEmail] = React.useState("");
-  const handleSubmit = () => {};
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // this is the firebase stuff
+    const config = {
+      url: process.env.REACT_APP_REGISTER_REDIRECT_URL, // where that email link will redirect to
+      handleCodeInApp: true,
+    };
+    await auth.sendSignInLinkToEmail(email, config);
+     // this is the request sent to firebase
+    toast.success(
+      `Email is sent to ${email}. Click the link to complete your registeration in your email `
+    );
+
+    window.localStorage.setItem("emailForRegisteration", email); // storing the email so that user doesnt have to type it again
+
+    setEmail("");
+  };
 
   const registerForm = () => {
     return (
@@ -15,7 +33,9 @@ export default function Register() {
           onChange={(e) => setEmail(e.target.value)}
           autoFocus
         />
-        <button type="submit" className="btn btn-raised" >Register</button>
+        <button type="submit" className="btn btn-raised">
+          Register
+        </button>
       </form>
     );
   };
